62e88944a81e9c5b16aaec1445a17c2270967dbf
wangzelei <1150119303@qq.com>
2018-01-11 22:08:42 +0800

    Create Project

---
+++ MvcApplicationTestGuestBook/MvcApplicationTestGuestBook/Controllers/GuestBookController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplicationTestGuestBook.Models;
+
+namespace MvcApplicationTestGuestBook.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        private MvcApplicationTestGuestBookContext db = new MvcApplicationTestGuestBookContext();
+
+        //
+        // GET: /GuestBook/
+
+        public ActionResult Index()
+        {
+            return View(db.Guestbooks.ToList());
+        }
+
+        //
+        // GET: /GuestBook/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        //
+        // GET: /GuestBook/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /GuestBook/Create
+
+        [HttpPost]
+        public ActionResult Create(Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guestbooks.Add(guestbook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(guestbook);
+        }
+
+        //
+        // GET: /GuestBook/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        //
+        // POST: /GuestBook/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guestbook).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guestbook);
+        }
+
+        //
+        // GET: /GuestBook/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        //
+        // POST: /GuestBook/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            db.Guestbooks.Remove(guestbook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplicationTestGuestBook/MvcApplicationTestGuestBook/Models/Guestbook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplicationTestGuestBook.Models
+{
+    public class Guestbook
+    {
+        public int Id { get; set; }
+
+        [DisplayName("姓名")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("邮箱")]
+        [Required]
+        public string Email { get; set; }
+        
+        [DisplayName("描述")]
+        [Required]
+        public string Memo { get; set; }
+    }
+}
